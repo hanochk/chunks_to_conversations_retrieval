70c2faeb2dacb775a407e49d0f49b50c20a0308a
h00633314 <hanoch.kremer@gmail.com>
2023-12-01 12:27:03 +0200

    Adding clean_noisy_segment, remove emojis, accronyms and Replacing the “:” following the speaker's name by the actual action “said”

--- main.py
+++ main.py
@@ -1,33 +1,24 @@
 import sys
 
-import numpy
-import requests
 import os
 from scipy.optimize import linear_sum_assignment
 import pickle
 # import matplotlib
 # matplotlib.use('TkAgg')
 # matplotlib.use('qtagg')
-import ipympl
 import matplotlib.pyplot as plt
 import re
-import json
 import copy
-import subprocess
 
 import numpy as np
 import torch
 import tqdm
-np.seterr(all='raise')
-# import spacy
-# import nltk
-# from spacy_wordnet.wordnet_annotator import WordnetAnnotator
+# np.seterr(all='raise')
 from sentence_transformers import SentenceTransformer
 
 import pandas as pd
-from sklearn.preprocessing import label_binarize
-from sklearn.metrics import precision_recall_curve
-from sklearn.metrics import average_precision_score
+import emoji
+from abbrev_dict import *
 from eval_metrices import roc_plot, p_r_plot
 def flatten(lst):
     return [x for l in lst for x in l]
@@ -43,57 +34,51 @@ def triplet_from_rel(rel):
 
 class SimilarityManager:
     def __init__(self):
-        # nlp = spacy.load('en_core_web_sm')
-        # nlp = spacy.load('en_core_web_lg')
-        # nlp.add_pipe("spacy_wordnet", after='tagger')
-        # nlp.add_pipe("spacy_wordnet", after='tagger', config={'lang': nlp.lang})
-        # self.nlp = nlp
         # self.similarity_model = SentenceTransformer('sentence-transformers/paraphrase-xlm-r-multilingual-v1')
         self.similarity_model = SentenceTransformer('all-MiniLM-L6-v2')
 
-        # SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2') # more recent
         if torch.cuda.device_count() > 0:
             self.similarity_model.cuda()
 
-    def similarity(self, src, target):
-        rc = []
-        s1 = self.nlp(src)
-        s2 = self.nlp(target)
-        for w in s1:
-            if w.pos_ not in ['NOUN', 'ADJ', 'ADV', 'VERB', 'PROPN'] and len(s1) > 1:
-                continue
-            rc.append(max([w.similarity(x) for x in s2]))
-        return np.mean(rc)
+    # def similarity(self, src, target):
+    #     rc = []
+    #     s1 = self.nlp(src)
+    #     s2 = self.nlp(target)
+    #     for w in s1:
+    #         if w.pos_ not in ['NOUN', 'ADJ', 'ADV', 'VERB', 'PROPN'] and len(s1) > 1:
+    #             continue
+    #         rc.append(max([w.similarity(x) for x in s2]))
+    #     return np.mean(rc)
 
     # def compare_cross_synsets(self, text1, text2):
     #     t1 = self.nlp(text1)
     #     t2 = self.nlp(text2)
     #     return compare_cross_lists([x._.wordnet.synsets() for x in t1], [x._.wordnet.synsets() for x in t2])
 
-    def compare_triplet(self, t1, t2, method='bert', invert_src=False, invert_dst=False, **kwargs):
-        if len(t1) != len(t2):
-            return 0.
-        sim = 1.
-        if method == 'bert':
-            if len(t1) == 2:  # attribute tuple, invert
-                if invert_src:
-                    t1 = [t1[1], t1[0]]
-                if invert_dst:
-                    t2 = [t2[1], t2[0]]
-            embs = self.similarity_model.encode([' '.join(t1).lower(), ' '.join(t2).lower()])
-            sim = cosine_sim(*embs)
-        elif method == 'meteor':
-            return nltk.translate.meteor_score.single_meteor_score(' '.join(t1).lower().split(),
-                                                                   ' '.join(t2).lower().split())
-        else:
-            for x, y in zip(t1, t2):
-                if method == 'wordnet':
-                    sim *= self.compare_cross_synsets(x, y)
-                elif method == 'spacy':
-                    sim *= self.similarity(x, y)
-                else:
-                    print("Unknown similarity method: {}".format(method))
-        return sim
+    # def compare_triplet(self, t1, t2, method='bert', invert_src=False, invert_dst=False, **kwargs):
+    #     if len(t1) != len(t2):
+    #         return 0.
+    #     sim = 1.
+    #     if method == 'bert':
+    #         if len(t1) == 2:  # attribute tuple, invert
+    #             if invert_src:
+    #                 t1 = [t1[1], t1[0]]
+    #             if invert_dst:
+    #                 t2 = [t2[1], t2[0]]
+    #         embs = self.similarity_model.encode([' '.join(t1).lower(), ' '.join(t2).lower()])
+    #         sim = cosine_sim(*embs)
+    #     elif method == 'meteor':
+    #         return nltk.translate.meteor_score.single_meteor_score(' '.join(t1).lower().split(),
+    #                                                                ' '.join(t2).lower().split())
+    #     else:
+    #         for x, y in zip(t1, t2):
+    #             if method == 'wordnet':
+    #                 sim *= self.compare_cross_synsets(x, y)
+    #             elif method == 'spacy':
+    #                 sim *= self.similarity(x, y)
+    #             else:
+    #                 print("Unknown similarity method: {}".format(method))
+    #     return sim
 
 
 def greedy_match(similarity_matrix):
@@ -115,8 +100,6 @@ def hungarian_match(similarity_matrix):
 
 class VGEvaluation:
     def __init__(self):
-        # nltk.download('wordnet')
-        # nltk.download('omw-1.4')
         self.smanager = SimilarityManager()
 
     def recall_triplet(self, src, dst, **kwargs):
@@ -211,7 +194,7 @@ class VGEvaluation:
             total_recall.extend(self.recall_triplets(src_i, dst_i, method=methods[i - 1]))
         return total_recall
 
-def training_zs(df_ref, result_dir, evaluator):
+def training_zs(df_ref, result_dir: str, evaluator, clean_noisy_segment: bool=True):
     max_seq_len = int(evaluator.smanager.similarity_model.max_seq_length *0.6)#WordPeice tokens to
     embds_seg_dialog_summ = list()
     all_embs_chunks = list()
@@ -226,7 +209,9 @@ def training_zs(df_ref, result_dir, evaluator):
         chunks = flatten([t.strip().split('.') for t in summ.strip().split(',')])[:-1]
         chunks = [c.strip() for c in chunks]
         all_chunk.append(chunks)
-        all_segmented_dialog = paragraph_seg(dialog, max_seq_len)
+        # if 1:
+        #     dialog = ' '.join([convert_abbrev(x) for x in dialog.split(' ')])
+        all_segmented_dialog = paragraph_seg(dialog, max_seq_len, clean_noisy_segment=clean_noisy_segment)
         all_seg_dialogs.append(all_segmented_dialog)
 
         embs_chunks = evaluator.encode_tokens(chunks)
@@ -241,25 +226,25 @@ def training_zs(df_ref, result_dir, evaluator):
         dialog_2_seg_id[idx] = list(np.arange(len_dialog_seg_prev, len_dialog_seg_prev + len(all_segmented_dialog)))
         len_dialog_seg_prev += len(all_segmented_dialog)
 
-
-    with open(os.path.join(result_dir, 'training_embds.pkl'), 'wb') as f:
+    tag = ['norm_noisy_segment_' if clean_noisy_segment else ''][0]
+    with open(os.path.join(result_dir, tag + 'training_embds.pkl'), 'wb') as f:
         pickle.dump((embds_seg_dialog_summ, all_embs_chunks), f)
 
 
-    with open(os.path.join(result_dir, 'training_chunk_2_summ_id.pkl'), 'wb') as f:
+    with open(os.path.join(result_dir, tag + 'training_chunk_2_summ_id.pkl'), 'wb') as f:
         pickle.dump(chunk_2_summ_id, f)
 
-    with open(os.path.join(result_dir, 'training_dialog_2_seg_id.pkl'), 'wb') as f:
+    with open(os.path.join(result_dir, tag + 'training_dialog_2_seg_id.pkl'), 'wb') as f:
         pickle.dump(dialog_2_seg_id, f)
 
-    with open(os.path.join(result_dir, 'training_all_chunk.pkl'), 'wb') as f:
+    with open(os.path.join(result_dir, tag + 'training_all_chunk.pkl'), 'wb') as f:
         pickle.dump(all_chunk, f)
 
-    with open(os.path.join(result_dir, 'training_all_seg_dialogs.pkl'), 'wb') as f:
+    with open(os.path.join(result_dir, tag  +'training_all_seg_dialogs.pkl'), 'wb') as f:
         pickle.dump(all_seg_dialogs, f)
 
 
-def create_dialog_seg_embds(dialogue, result_dir, evaluator, tag='test'):
+def create_dialog_seg_embds(dialogue, result_dir, evaluator, clean_noisy_segment, tag='test'):
     max_seq_len = int(evaluator.smanager.similarity_model.max_seq_length *0.6)#WordPeice tokens to
 
     embds_seg_dialog_summ = list()
@@ -269,7 +254,7 @@ def create_dialog_seg_embds(dialogue, result_dir, evaluator, tag='test'):
     all_seg_dialogs = list()
 
     for idx, (dialog) in enumerate(tqdm.tqdm(dialogue)):
-        all_segmented_dialog = paragraph_seg(dialog, max_seq_len)
+        all_segmented_dialog = paragraph_seg(dialog, max_seq_len, clean_noisy_segment=clean_noisy_segment)
         all_seg_dialogs.append(all_segmented_dialog)
         segmented_dialog_embs = evaluator.encode_tokens(all_segmented_dialog)
         embds_seg_dialog_summ.append(segmented_dialog_embs)
@@ -288,10 +273,11 @@ def create_dialog_seg_embds(dialogue, result_dir, evaluator, tag='test'):
     with open(os.path.join(result_dir, tag + '_all_seg_dialogs.pkl'), 'wb') as f:
         pickle.dump(all_seg_dialogs, f)
 
-def paragraph_seg(dialog, max_seq_len):
+def paragraph_seg(dialog: str, max_seq_len: int, clean_noisy_segment: bool=True):
     all_segmented_dialog = list()
     ptr_beg = 0
     ptr_end = 0
+    all_dirty_window = list()
     while ptr_beg < len(dialog):
         delimiter = [(m.start(), m.end()) for m in re.finditer('\r', dialog[ptr_beg:]) if m.start() < max_seq_len]
         if delimiter != []:
@@ -301,10 +287,23 @@ def paragraph_seg(dialog, max_seq_len):
 
         if ptr_end <= 1: # when no delimiter in the range then brute force
             ptr_end = max_seq_len # brute force
-        window_dialog = re.sub('\r\n', ' ', dialog[ptr_beg:ptr_beg + ptr_end]).strip()
+
+        if clean_noisy_segment:
+            window_dialog = re.sub('\r\n', ' . ', dialog[ptr_beg:ptr_beg + ptr_end]).strip()
+            all_dirty_window.append(window_dialog)
+            window_dialog = ' '.join([remove_abbrev(x) for x in window_dialog.split(' ')])
+            window_dialog = ' '.join([remove_abbrev_post_comma(x) for x in window_dialog.split(' ')])
+            window_dialog = ' '.join([remove_abbrev_post_period(x) for x in window_dialog.split(' ')])
+            # window_dialog = remove_abbrev_dialog(window_dialog)
+            window_dialog = window_dialog.replace(': ', ' said ') # replace : after name with action
+            window_dialog = window_dialog.replace(' . ', ' ') # replace : after name with action
+
+        else:
+            window_dialog = re.sub('\r\n', ' ', dialog[ptr_beg:ptr_beg + ptr_end]).strip()
+
         ptr_beg += ptr_end
         all_segmented_dialog.append(window_dialog)
-
+        # [(x, y) for x, y in zip(all_segmented_dialog, all_dirty_window)]
     return all_segmented_dialog
 
 def embeddings_extract(sentence: list, key_tag:str , result_dir: str, evaluator):
@@ -375,83 +374,31 @@ def main():
     # Use a breakpoint in the code line below to debug your script.
     result_dir = r'C:\Users\h00633314\HanochWorkSpace\Projects\chunk_back_to_summary\chunks_to_conversations\bin'
     evaluator = VGEvaluation()
-    analyse_reference = False
-    train_data = False
+
+    # analyse_reference = False
+    train_data = True
     pre_compute_embeddings = False
+    clean_noisy_segment = False
+    denoise_tag = ['norm_noisy_segment_' if clean_noisy_segment else ''][0]
 
     print("Max Sequence Length:", evaluator.smanager.similarity_model.max_seq_length)
 
-    if analyse_reference:
-        if 1:
-            df_ref = pd.read_csv('reference.csv')
-            training_zs(df_ref, result_dir, evaluator)
-        else:
-            evaluation_ac(result_dir=result_dir, pkl_file='training.pkl')
-        return
+    # if analyse_reference:
+    #     if 1:
+    #         df_ref = pd.read_csv('reference.csv')
+    #         training_zs(df_ref, result_dir, evaluator)
+    #     else:
+    #         evaluation_ac(result_dir=result_dir, pkl_file='training.pkl')
+    #     return
 # TODO check "" vs. ''
     if pre_compute_embeddings:
-        if train_data:
-            df_ref = pd.read_csv('reference.csv')
-            training_zs(df_ref, result_dir, evaluator)
-
-
-        else: # test data
-            df_chunks = pd.read_csv('summary_pieces.csv')
-            df_dialog = pd.read_csv('dialogues.csv')
-            dialog_list = df_dialog['dialogue'].to_list()
-            chunk_list = df_chunks['summary_piece'].to_list()
-            # key_tag_d = 'dialogue'
-            key_tag_chunk = 'test_chunks'
-
-            # all_embds_dialog = embeddings_extract(dialog_list,
-            #                     key_tag=key_tag_d,
-            #                     result_dir=result_dir,
-            #                     evaluator=evaluator)
-            create_dialog_seg_embds(dialog_list, result_dir, evaluator)
-
-            all_embds_chunks = embeddings_extract(chunk_list,
-                                key_tag=key_tag_chunk,
-                                result_dir=result_dir,
-                                evaluator=evaluator)
+        compute_embeddings(clean_noisy_segment, evaluator, result_dir, train_data)
+        return
     else:
-        if train_data:
-            # key_tag_d = 'dialogue_train'
-            # key_tag_chunk = 'chunks_train'
-            with open(os.path.join(result_dir, 'training_chunk_2_summ_id.pkl'), 'rb') as f:
-                chunk_2_summ_id = pickle.load(f)
-            with open(os.path.join(result_dir, 'training_dialog_2_seg_id.pkl'), 'rb') as f:
-                dialog_2_seg_id = pickle.load(f)
-
-            with open(os.path.join(result_dir, 'training_embds.pkl'), 'rb') as f:
-                (all_embds_seg_dialog, all_embds_chunks) = pickle.load(f)
-
-            with open(os.path.join(result_dir, 'training_all_chunk.pkl'), 'rb') as f:
-                all_chunk = pickle.load(f)
-
-            with open(os.path.join(result_dir, 'training_all_seg_dialogs.pkl'), 'rb') as f:
-                all_seg_dialogs = pickle.load(f)
-
-
-        else:
-            key_tag_chunk = 'test_chunks'
-            tag = 'test'
-
-            with open(os.path.join(result_dir, str(key_tag_chunk) + '.pkl'), 'rb') as f:
-                all_embds_chunks = pickle.load(f)
-
-            with open(os.path.join(result_dir, tag + '_embds.pkl'), 'rb') as f:
-                all_embds_seg_dialog = pickle.load(f)
-
-            with open(os.path.join(result_dir, tag + '_dialog_2_seg_id.pkl'), 'rb') as f:
-                dialog_2_seg_id = pickle.load(f)
-
-            with open(os.path.join(result_dir, tag + '_all_seg_dialogs.pkl'), 'rb') as f:
-                all_seg_dialogs = pickle.load(f)
-
-            df_chunks = pd.read_csv('summary_pieces.csv')
-            all_chunk = df_chunks['summary_piece'].to_list()
+        all_chunk, all_embds_chunks, all_embds_seg_dialog, all_seg_dialogs, chunk_2_summ_id, dialog_2_seg_id = load_pre_computed_embeddings(
+            denoise_tag, result_dir, train_data)
     if 0:
-        with open(os.path.join(result_dir, 'training_bipartite_matching.pkl'), 'rb') as f:
+        with open(os.path.join(result_dir, denoise_tag + 'training_bipartite_matching.pkl'), 'rb') as f:
             res = pickle.load(f)
 
     else:
@@ -463,11 +410,6 @@ def main():
                                                         embds_chunks = all_embds_chunks,
                                                         embds_seg_dialog = all_embds_seg_dialog)
 
-    # pr_gpt, re_gpt = evaluator.compute_precision_recall(df_chunks['summary_piece'].to_list(),
-    #                                                     df_dialog['dialogue'].to_list(),
-    #                                                     assignment_method='hungarian',
-    #                                                     debug_print=True,
-    #                                                     chunk_2_paragraph=True)
     if train_data:
         tp = 0
         all_errors = list()
@@ -491,9 +433,9 @@ def main():
         print("Recall {}".format(tp/len(res)))
 
         pd.DataFrame.from_dict(chunk_2_dialog_match, orient='index').transpose().to_csv(
-            os.path.join(result_dir, 'training_bipartite_matching_results.csv'))
+            os.path.join(result_dir, denoise_tag + 'training_bipartite_matching_results.csv'))
 
-        with open(os.path.join(result_dir, 'training_bipartite_matching.pkl'), 'wb') as f:
+        with open(os.path.join(result_dir, denoise_tag + 'training_bipartite_matching.pkl'), 'wb') as f:
             pickle.dump(res, f)
 
     else:
@@ -506,13 +448,78 @@ def main():
                 chunk_2_dialog_match[dialog_ind].append(all_chunk[tup[0]])
 
         pd.DataFrame.from_dict(chunk_2_dialog_match, orient='index').to_csv(
-            os.path.join(result_dir, 'test_bipartite_matching_results.csv'))
+            os.path.join(result_dir, 'results', denoise_tag + 'test_bipartite_matching_results.csv'))
 
-        with open(os.path.join(result_dir, 'test_bipartite_matching.pkl'), 'wb') as f:
+        with open(os.path.join(result_dir, denoise_tag + 'test_bipartite_matching.pkl'), 'wb') as f:
             pickle.dump(res, f)
 
 
-    print('ka')
+def load_pre_computed_embeddings(denoise_tag, result_dir, train_data):
+    if train_data:
+        # key_tag_d = 'dialogue_train'
+        # key_tag_chunk = 'chunks_train'
+        with open(os.path.join(result_dir, denoise_tag + 'training_chunk_2_summ_id.pkl'), 'rb') as f:
+            chunk_2_summ_id = pickle.load(f)
+        with open(os.path.join(result_dir, denoise_tag + 'training_dialog_2_seg_id.pkl'), 'rb') as f:
+            dialog_2_seg_id = pickle.load(f)
+
+        with open(os.path.join(result_dir, denoise_tag + 'training_embds.pkl'), 'rb') as f:
+            (all_embds_seg_dialog, all_embds_chunks) = pickle.load(f)
+
+        with open(os.path.join(result_dir, denoise_tag + 'training_all_chunk.pkl'), 'rb') as f:
+            all_chunk = pickle.load(f)
+
+        with open(os.path.join(result_dir, denoise_tag + 'training_all_seg_dialogs.pkl'), 'rb') as f:
+            all_seg_dialogs = pickle.load(f)
+
+
+    else:
+        key_tag_chunk = 'test_chunks'
+        tag = 'test'
+
+        with open(os.path.join(result_dir, denoise_tag + str(key_tag_chunk) + '.pkl'), 'rb') as f:
+            all_embds_chunks = pickle.load(f)
+
+        with open(os.path.join(result_dir, denoise_tag + tag + '_embds.pkl'), 'rb') as f:
+            all_embds_seg_dialog = pickle.load(f)
+
+        with open(os.path.join(result_dir, denoise_tag + tag + '_dialog_2_seg_id.pkl'), 'rb') as f:
+            dialog_2_seg_id = pickle.load(f)
+
+        with open(os.path.join(result_dir, denoise_tag + tag + '_all_seg_dialogs.pkl'), 'rb') as f:
+            all_seg_dialogs = pickle.load(f)
+
+        df_chunks = pd.read_csv('summary_pieces.csv')
+        all_chunk = df_chunks['summary_piece'].to_list()
+    return all_chunk, all_embds_chunks, all_embds_seg_dialog, all_seg_dialogs, chunk_2_summ_id, dialog_2_seg_id
+
+
+def compute_embeddings(clean_noisy_segment, evaluator, result_dir, train_data):
+    if train_data:
+        df_ref = pd.read_csv('reference.csv')
+        training_zs(df_ref, result_dir, evaluator, clean_noisy_segment=clean_noisy_segment)
+
+
+    else:  # test data
+        df_chunks = pd.read_csv('summary_pieces.csv')
+        df_dialog = pd.read_csv('dialogues.csv')
+        dialog_list = df_dialog['dialogue'].to_list()
+        chunk_list = df_chunks['summary_piece'].to_list()
+        # key_tag_d = 'dialogue'
+        key_tag_chunk = 'test_chunks'
+
+        # all_embds_dialog = embeddings_extract(dialog_list,
+        #                     key_tag=key_tag_d,
+        #                     result_dir=result_dir,
+        #                     evaluator=evaluator)
+        create_dialog_seg_embds(dialog_list, result_dir, evaluator, clean_noisy_segment=clean_noisy_segment)
+
+        all_embds_chunks = embeddings_extract(chunk_list,
+                                              key_tag=key_tag_chunk,
+                                              result_dir=result_dir,
+                                              evaluator=evaluator)
+        return all_embds_chunks
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
